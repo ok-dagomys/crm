c9cc221777bcba6ec7fccd88cc7f3f5081c009b9
Divider <it-cio@mail.ru>
2022-09-29 09:00:47 +0300

    Add purchase tasks

---
+++ tasks/purchase.py
@@ -0,0 +1,119 @@
+import asyncio
+import os
+from datetime import datetime
+
+import aspose.words as aw
+import docx
+import pandas as pd
+from dotenv import load_dotenv
+
+from sql import add_to_db
+
+load_dotenv()
+task_source = os.getenv('TASK_SOURCE')
+task_registry = os.getenv('TASK_REGISTRY')
+task_archive = os.getenv('TASK_ARCHIVE')
+file_list = []
+
+
+def convert_to_docx(file_doc, c_time, m_time):
+    file_docx = file_doc.rsplit('.', 1)[0] + '.docx'
+    new_filename = os.path.join(task_source, file_docx)
+    old_filename = os.path.join(task_source, file_doc)
+    doc = aw.Document(old_filename)
+    doc.save(new_filename)
+    os.remove(f'{task_source}/{file_doc}')
+    os.utime(f'{task_source}/{file_docx}', (c_time, m_time))
+    return file_docx
+
+
+def make_file_list(f_docx, c_date):
+    doc = docx.Document(f'{task_source}/{f_docx}')
+    cost = 'Стоимость не указана'
+    for paragraph in doc.paragraphs:
+        if 'Предположительная стоимость заявки:' in paragraph.text:
+            cost = paragraph.text.split(':')[-1].strip()
+
+    file_docx = os.path.splitext(f_docx)[0]
+    if '-' in file_docx:
+        status = 'заявка запланирована в работу'
+    elif file_docx.count('+') == 1:
+        status = 'заявка подписана и передана в мто'
+    elif file_docx.count('+') == 2:
+        status = 'подготовлен договор и счет'
+    elif file_docx.count('+') == 3:
+        status = 'счет подписан и передан в оплату'
+    elif file_docx.count('+') == 4:
+        status = 'товар поставлен'
+    elif '=' in file_docx:
+        status = 'заявка готовится к торгам'
+    else:
+        status = 'статус не присвоен'
+
+    file_list.append([file_docx.lower(), c_date.strftime('%Y.%m.%d'), cost, status])
+
+
+def write_xlsx(dataframe):
+    with pd.ExcelWriter(f'{task_registry}/{datetime.now().year}.xlsx', engine='xlsxwriter') as wb:
+        dataframe.to_excel(wb, sheet_name='Реестр', index=False)
+        sheet = wb.sheets['Реестр']
+
+        sheet.set_column('A:A', 50)
+        sheet.set_column('B:B', 10)
+        sheet.set_column('C:C', 20)
+        sheet.set_column('D:D', 33)
+
+
+def add_to_registry(f_list):
+    df = pd.DataFrame(f_list, columns=['Заявка', 'Дата', 'Стоимость', 'Статус'])\
+        .drop_duplicates()\
+        .sort_values(by='Статус', ascending=True)\
+        .dropna()\
+        .reset_index(drop=True)
+
+    if os.path.isfile(f'{task_registry}/{datetime.now().year}.xlsx'):
+        n_list = pd.read_excel(f'{task_registry}/{datetime.now().year}.xlsx')\
+                .dropna()\
+                .values.tolist()
+
+        if sorted(f_list) != sorted(n_list):
+            write_xlsx(df)
+    else:
+        write_xlsx(df)
+
+
+def transfer_to_archive(f_docx, f_date):
+    if not os.path.isdir(f'{task_archive}/{f_date.year}'):
+        os.mkdir(f'{task_archive}/{f_date.year}')
+    os.replace(f'{task_source}/{f_docx}',
+               f'{task_archive}/{f_date.year}/{f_date.strftime("%Y.%m.%d")} - {f_docx.split("+", 5)[-1].strip()}')
+    add_to_db(f'Заявка {f_docx.split("+", 5)[-1].strip()} перемещена в архив')
+
+
+async def scan_tasks():
+    for file in os.listdir(task_source):
+        created_time = os.path.getctime(f'{task_source}/{file}')
+        modified_time = os.path.getmtime(f'{task_source}/{file}')
+        date = datetime.fromtimestamp(modified_time)
+
+        if file.lower().endswith('.doc') and '~' not in file:
+            file = convert_to_docx(file, created_time, modified_time)
+            make_file_list(file, date)
+
+        elif file.lower().endswith('.docx') and '~' not in file:
+            if file.count('+') < 5:
+                make_file_list(file, date)
+            elif file.count('+') == 5:
+                transfer_to_archive(file, date)
+
+    add_to_registry(file_list)
+    print(f'Дата последней проверки: {datetime.now().strftime("%Y.%m.%d в %H:%M:%S")}')
+
+
+async def check_task():
+    task = asyncio.create_task(scan_tasks())
+    await task
+    await asyncio.sleep(0.1)
+
+
+asyncio.run(check_task())
